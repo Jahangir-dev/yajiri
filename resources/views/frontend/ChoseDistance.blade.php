ba911ad5434e225d9cb67cf0824b7bbcea8ac511
sooraj384 <sirajulhaq363@gmail.com>
2022-05-12 21:58:45 +0500

    lat lng

--- resources/views/frontend/ChoseDistance.blade.php
+++ resources/views/frontend/ChoseDistance.blade.php
@@ -171,7 +171,9 @@
 var circle;
         function initMap() {
 
-
+lat = $('#lat').val();
+lng = $('#lng').val();
+console.log(lat,lng);
           map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: lat, lng: lng },
             zoom: 12,
