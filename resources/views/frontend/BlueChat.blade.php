8e17a607ddb3b735bfd1e19828a73c2439859f76
jahangir <>
2022-05-18 23:18:53 +0500

    bluechat

--- resources/views/frontend/BlueChat.blade.php
+++ resources/views/frontend/BlueChat.blade.php
@@ -282,7 +282,7 @@
                         <div class="row mb-2">
                             <div class="col d-flex">
                                 <img src="{{asset('icons/ic_place_-8.png')}}" style="width: 1rem; height: 1.5rem;">
-                                <p class="ReviewText1">{{$user->address}}</p>
+                                <p class="ReviewText1">{{isset($user->address)}}</p>
                             </div>
                         </div>
                         <div class="row mb-2">
@@ -294,7 +294,7 @@
                         <div class="row mb-3">
                             <div class="col d-flex">
                                 <img src="{{asset('icons/ic_date_range_-1.png')}}" style="width: 1rem; height: 1.2rem;">
-                                <p class="ReviewText1">{{$user->created_at->diffForHumans()}}</p>
+                                <p class="ReviewText1">{{isset($user->created_at->diffForHumans())}}</p>
                             </div>
                         </div>
                         <div class="row mb-4">
